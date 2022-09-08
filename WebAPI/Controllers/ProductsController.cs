93230ff19abd50e2f9f147f242d28573e9156153
Victor Manuel Chacon Melendez <victormanuel230290@gmail.com>
2022-09-07 20:11:45 -0600

    Agregar archivos de proyecto.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,109 @@
+﻿using ClassLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private LocalDBMSSQLLocalDBContext _database;
+        public ProductsController(LocalDBMSSQLLocalDBContext database)
+        {
+            _database = database;
+        }
+
+        /// <summary>
+        /// Get product list
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetProductList")]
+        public async Task<List<ProductItem>> GetProductList()
+        {
+            return await _database.ProductItem.ToListAsync();
+        }
+
+        /// <summary>
+        /// Get product by it's Identidier
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetProductById")]
+        public async Task<ProductItem> GetProductById(int id)
+        {
+            return await _database.ProductItem.Where( x => x.Id == id ).FirstOrDefaultAsync();
+        }
+
+        /// <summary>
+        /// Add new product
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("SaveProductItem")]
+        public async Task<ActionResult> SaveProductItem( ProductItem product )
+        {
+            var entry = _database.Entry(product);
+
+            _database.ProductItem.Add(product);
+            await _database.SaveChangesAsync();
+                       
+
+            if (entry.State == EntityState.Added)
+                return Ok();
+            else
+                return BadRequest();
+        }
+
+        /// <summary>
+        /// Update product information
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("UpdateProductItem")]
+        public async Task<ActionResult> UpdateProductItem(ProductItem product)
+        {
+            var dbItem = _database.ProductItem.SingleOrDefault( x => x.Id == product.Id );
+
+            var entry = _database.Entry(dbItem);
+            entry.CurrentValues.SetValues(product);
+
+            await _database.SaveChangesAsync();
+
+            if (entry.State == EntityState.Modified)
+                return Ok();
+            else
+                return BadRequest();
+        }
+
+        /// <summary>
+        /// Delete product by it's identidfier
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("DeleteProductById")]
+        public async Task<ActionResult> DeleteProductById(int id)
+        {
+            var dbItem = _database.ProductItem.SingleOrDefault(x => x.Id == id);
+
+            _database.ProductItem.Remove(dbItem);
+            await _database.SaveChangesAsync();
+
+            if (_database.Entry(dbItem).State == EntityState.Deleted)
+                return Ok();
+            else
+                return BadRequest();
+        }
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,69 @@
+using ClassLibrary.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //GET DATABASE FILE DIRECTORY
+            var parent = System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).FullName;
+            var path = Path.Combine(parent, @"ClassLibrary\DataBaseFile\ProductsDB.mdf");
+
+            services.AddDbContext<LocalDBMSSQLLocalDBContext>(options =>
+                options.UseSqlServer(
+                    string.Format(Configuration["ConnectionStrings:DefaultConnection"], path)), ServiceLifetime.Transient);
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI Softwareentwicklung", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI Softwareentwicklung"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
